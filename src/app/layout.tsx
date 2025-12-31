f6f91acf782e98da8bfb5fa914f9e88bdffc5c70
958877748 <31437616+958877748@users.noreply.github.com>
2026-01-01 00:36:35 +0800

    移除 ClerkProvider 废弃属性

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,6 @@ export default function RootLayout({
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      afterSignInUrl="/"
-      afterSignUpUrl="/"
       appearance={{
         elements: {
           card: {
